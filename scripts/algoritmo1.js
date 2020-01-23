e5781a0f5c10341261754941870c871e02d4922c
Aldair <aldair17rodriguezt@gmail.com>
2020-01-23 16:42:19 -0500

    se avanzo el primer ejercicio y una parte del segundo

--- scripts/algoritmo1.js
+++ scripts/algoritmo1.js
@@ -1,2 +1,12 @@
 //Implementar un algoritmo que reciba 2 argumentos y los sume, el resultado se
 //debera imprimir en pantalla.
+
+function sumarNumeros(a,b){
+       let suma = a+b; 
+       console.log(suma);
+}
+
+let primerNumero = parseInt(prompt("ingrese el primer numero"));
+let segundoNumero = parseInt(prompt("ingrese el segundo numero"));
+ 
+sumarNumeros(primerNumero,segundoNumero);
---
+++ scripts/algoritmo2.js
@@ -0,0 +1,5 @@
+//Un estudiante realiza 4 examenes, calcular el promedio de estos.
+function getPromedio(nota1,nota2,nota3,nota4){
+     let suma = nota1+nota2+nota3+nota4;
+     
+}
